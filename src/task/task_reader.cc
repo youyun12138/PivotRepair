6a4c98b3e03b78f96929dc6f94cdb606374bf20e
uyun <2844275963@qq.com>
2023-07-07 17:49:44 +0800

    taskreader annotation

--- src/task/task_reader.cc
+++ src/task/task_reader.cc
@@ -44,19 +44,31 @@ Count TaskReader::GetTaskNumber(const Count &gid) {
   return gid == 0 ? task_num_ : 0;
 }
 
+// gid：类型为Count，表示全局ID。
+// tid：类型为Count，表示任务ID。
+// node_id：类型为Count，表示节点ID。
+// rt：类型为RepairTask的引用，表示需要填充的修复任务对象。
+// src_ids：类型为Count*的指针，表示源ID列表。
+
+//从一个任务中获取特定节点的修复任务信息，并将其填充到RepairTask对象中。
 void TaskReader::FillTask(const Count &gid, const Count &tid,
                           const Count &node_id,
                           RepairTask &rt, Count *src_ids) {
+  // 通过任务ID获取任务信息，存储在auto &task中。
   auto &task = task_infos_[tid];
   rt.tar_id = 0;
   rt.src_num = 0;
+  //遍历任务中的所有节点任务，；
   for (Count i = 0; i < task.node_num; ++i) {
     auto &ntask = task.node_tasks[i];
+    //对于每个节点任务，如果其node_id等于node_id，则将其tar_id赋值给rt.tar_id
     if (ntask.node_id == node_id)
       rt.tar_id = ntask.tar_id;
+    // 否则，如果其tar_id等于node_id，则将其node_id添加到src_ids中，并将rt.src_num自增。
     else if (ntask.tar_id == node_id)
       src_ids[(rt.src_num)++] = ntask.node_id;
   }
+  //如果rt.tar_id等于0，则将rt.size赋值为0并返回。
   if (rt.tar_id == 0) {
     rt.size = 0;
     return;
